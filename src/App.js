225514a56ee2b66a8794271e90a2a955e117a410
pzync <shkhrgrv@gmail.com>
2019-05-17 20:47:03 +0530

    #7.1 Animated Nav working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import logo from "./logo.svg";
 import "./App.css";
-import Toggle from "./toggle";
+import Toggle from "./Toggle";
+import Nav from "./Nav";
 
 function App() {
   const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
+  const [navOpen, setNavOpen] = useState(false);
+  const navAnimation = useSpring({
+    transform: navOpen ? `translate3d(0,0,0)` : `translate3d(100%,0,0)`
+  });
 
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt="logo" />
-        <button className="menu-button">Menu</button>
+        <button className="menu-button" onClick={() => setNavOpen(!navOpen)}>
+          Menu
+        </button>
       </header>
+      <Nav style={navAnimation} />
       <main>
         <Toggle />
       </main>
