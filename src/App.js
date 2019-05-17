6212948d59c460ed9f450daa85f3796052697ef1
pzync <shkhrgrv@gmail.com>
2019-05-17 17:54:22 +0530

    #3 A Basic spring completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { useSpring, animated } from "react-spring";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" alt="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
+
+  return (
+    <animated.div className="App" style={fade}>
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button className="menu-button">Menu</button>
+      </header>
+    </animated.div>
+  );
 }
 
 export default App;
