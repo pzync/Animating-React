1d729b869c28bf12c25fc1e745de1500c3754c71
pzync <shkhrgrv@gmail.com>
2019-05-17 17:33:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="logo" />
+          <img src={logo} className="logo" alt="logo" />
           <button className="menu-button">Menu</button>
         </header>
       </div>
