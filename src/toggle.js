9198b4f66c488acf86d80650b2a9b980f90d3498
pzync <shkhrgrv@gmail.com>
2019-05-18 13:14:30 +0530

    11.1 Toggle between components via useTransition

--- src/toggle.js
+++ src/toggle.js
@@ -5,20 +5,23 @@ const Toggle = () => {
   const [toggleState, setToggleState] = useState(false);
 
   const transitions = useTransition(toggleState, null, {
-    from: { opacity: 0 },
+    from: { opacity: 0, position: "absolute" },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
   });
 
   return (
-    <div>
-      {transitions.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
+    <div style={{ position: "relative" }}>
+      {transitions.map(({ item, key, props }) =>
+        item ? (
+          <animated.h1 key={key} style={props}>
+            Hello
+          </animated.h1>
+        ) : (
+          <animated.h1 key={key} style={props}>
+            World
+          </animated.h1>
+        )
       )}
 
       <button onClick={() => setToggleState(!toggleState)}>Toggle</button>
@@ -27,3 +30,29 @@ const Toggle = () => {
 };
 
 export default Toggle;
+
+// const [toggleState, setToggleState] = useState(false);
+
+// const transitions = useTransition(toggleState, null, {
+//   from: { opacity: 0, position: "absolute" },
+//   enter: { opacity: 1 },
+//   leave: { opacity: 0 }
+// });
+
+// return (
+//   <div style={{ position: "relative" }}>
+//     {transitions.map(({ item, key, props }) =>
+//       item ? (
+//         <animated.h1 key={key} style={props}>
+//           Hello
+//         </animated.h1>
+//       ) : (
+//         <animated.h1 key={key} style={props}>
+//           World
+//         </animated.h1>
+//       )
+//     )}
+
+//     <button onClick={() => setToggleState(!toggleState)}>Toggle</button>
+//   </div>
+// );
