327585dff870cbb34dc5bea8cfefeff14cf1f9eb
pzync <shkhrgrv@gmail.com>
2019-05-19 11:34:40 +0530

    #11.n Transition with Multiple Components with useTransition done

--- src/toggle.js
+++ src/toggle.js
@@ -1,30 +1,54 @@
 import React, { useState } from "react";
 import { useTransition, animated } from "react-spring";
 
-const Toggle = () => {
-  const [toggleState, setToggleState] = useState(false);
+const shuffleArray = array => {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
 
-  const transitions = useTransition(toggleState, null, {
-    from: { opacity: 0, position: "absolute" },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 }
+const Toggle = () => {
+  const [items, setItems] = useState([
+    {
+      id: 0,
+      text: "Hello World"
+    },
+    {
+      id: 1,
+      text: "Lorem Ipsum"
+    },
+    {
+      id: 2,
+      text: "Namaste India"
+    },
+    {
+      id: 3,
+      text: "Huzzah!"
+    }
+  ]);
+
+  const transitions = useTransition(items, item => item.id, {
+    from: { transform: `translateX(-20px)` },
+    enter: { transform: `translateX(0px)` },
+    leave: { transform: `translateX(20px)` }
   });
 
   return (
-    <div style={{ position: "relative" }}>
-      {transitions.map(({ item, key, props }) =>
-        item ? (
-          <animated.h1 key={key} style={props}>
-            Hello
-          </animated.h1>
-        ) : (
-          <animated.h1 key={key} style={props}>
-            World
-          </animated.h1>
-        )
-      )}
+    <div>
+      {transitions.map(({ item, key, props }) => (
+        <animated.h1 key={key} style={props}>
+          {item.text}
+        </animated.h1>
+      ))}
 
-      <button onClick={() => setToggleState(!toggleState)}>Toggle</button>
+      <button onClick={() => setItems({ id: 0, text: "Hello World" })}>
+        Toggle
+      </button>
     </div>
   );
 };
