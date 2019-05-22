9c19121e3f5491417d04f6be28dcdaeeacf6cf45
pzync <shkhrgrv@gmail.com>
2019-05-22 17:15:03 +0530

    21.1 useChain working

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,22 +1,48 @@
-import React, { useState } from "react";
-import { animated, useTrail } from "react-spring";
+import React, { useState, useRef } from "react";
+import { animated, useTrail, useSpring, useChain } from "react-spring";
 
-const items = [0, 1, 2, 3, 4];
+const items = [0, 1, 2, 3, 4, 5];
 
 const Boxes = () => {
   const [toggleState, setToggleState] = useState(false);
 
-  const trails = useTrail(items.length, {
-    opacity: toggleState ? 1 : 0,
-    transform: toggleState ? `scale(1)` : `scale(0.6)`
+  const springRef = useRef();
+  const { size } = useSpring({
+    ref: springRef,
+    from: { size: `20%` },
+    to: { size: toggleState ? `100%` : `20%` }
   });
 
+  const trailRef = useRef();
+  const trails = useTrail(items.length, {
+    ref: trailRef,
+    from: {
+      opacity: 0,
+      transform: `scale(0.6)`
+    },
+    to: {
+      opacity: toggleState ? 1 : 0,
+      transform: toggleState ? `scale(1)` : `scale(0.6)`
+    }
+  });
+
+  useChain(toggleState ? [springRef, trailRef] : [trailRef, springRef]);
+
   return (
-    <div className="boxes-grid">
-      <button onClick={() => setToggleState(!toggleState)}>Toggle</button>
-      {trails.map(animation => (
-        <animated.div key={animation.id} className="box" style={animation} />
-      ))}
+    <div className="full-height">
+      <animated.div
+        style={{ height: size, width: size }}
+        className="boxes-grid-two"
+        onClick={() => setToggleState(!toggleState)}
+      >
+        {trails.map(animation => (
+          <animated.div
+            key={animation.id}
+            className="box-two"
+            style={animation}
+          />
+        ))}
+      </animated.div>
     </div>
   );
 };
